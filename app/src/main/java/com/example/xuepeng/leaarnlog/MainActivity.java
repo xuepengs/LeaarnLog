b402c52e0245a523a4e3d1b39daf3f9180f4c8ac
xuepeng <helloxuepeng@gmail.com>
2018-03-22 00:26:54 +0800

    使用日志API

--- app/src/main/java/com/example/xuepeng/leaarnlog/MainActivity.java
+++ app/src/main/java/com/example/xuepeng/leaarnlog/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.xuepeng.leaarnlog;
 
+import android.nfc.Tag;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static String TAG="MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        System.out.println(" 普通信息");
+//        System.err.println(" 错误信息");
+        Log.d(TAG," 调试信息");
+        Log.e(TAG,"错误信息");
+        Log.i(TAG,"普通信息");
+        Log.v(TAG,"无用信息");
+        Log.w(TAG,"警告信息");
     }
 }
